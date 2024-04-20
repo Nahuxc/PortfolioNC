c570901844e7404536f59de77639cf8138a8d2d4
Nahuel <coronel98nahuel@gmail.com>
2024-04-19 23:24:10 -0300

    update: projects img

--- src/Components/Projects/ContentProject/ContentProject.jsx
+++ src/Components/Projects/ContentProject/ContentProject.jsx
@@ -7,7 +7,7 @@ import "./ContentProject.css"
 import imgA from "../../../../img/dvasistentesinmobiliarios.jpg"
 import imgB from "../../../../img/bookstorecourseshome.jpg"
 import imgC from "../../../../img/api-articles.png"
-import imgD from "../../../../img/NewsLetterProyect.png"
+import imgD from "../../../../img/NewsLettersGenerator.png"
 
 /* icons */
 import { FaCss3Alt } from "react-icons/fa";
@@ -30,57 +30,6 @@ const ContentProject = () => {
   return (
 
     <div className='box-projects'>
-      {/* card */}
-      <div className='box-card'>
-        <div className='box-card-content'>
-          <div className='box-img'>
-            <img src={imgB} alt="bookstorecourseshome" />
-          </div>
-          <div className='box-card-content_texts'>
-            <div className='box-card-content_texts-icons'>
-              <li className='card-icon'> <FaSass color='#ff06ac' /> </li>
-              <li className='card-icon'> <SiJavascript color='#b5a900' /> </li>
-              <li className='card-icon'> <FaCss3Alt color='#009dff' /> </li>
-              <li className='card-icon'> <FaHtml5 color='#ff430a' /> </li>
-            </div>
-            <div>
-              <h2>Book Store Cursos</h2>
-              <span className='text-span-card'>Desarrollo Web</span>
-              <p>Maquetado Desarrollado con el FrameWork SASS de CSS, Demuestra conocimientos en el desarrollo de diseño web y algunas funcionalidades con JS.</p>
-            </div>
-            <div className='box-card-content_texts-links'>
-              <a href="https://github.com/Nahuxc/Book-Store-cursos" target='_blank' className='card-iconlink'><FaCode color='#009dff' /></a>
-              <a href="https://nahuxc.github.io/Book-Store-cursos/" target='_blank' className='card-iconlink'><AiOutlineGlobal color='#009dff' /></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* card */}
-      <div className='box-card'>
-        <div className='box-card-content'>
-          <div className='box-img'>
-            <img src={imgA} alt="bookstorecourseshome" />
-          </div>
-          <div className='box-card-content_texts'>
-            <div className='box-card-content_texts-icons'>
-              <li className='card-icon'> <FaHtml5 color='#ff430a' /> </li>
-              <li className='card-icon'> <FaCss3Alt color='#009dff' /> </li>
-              <li className='card-icon'> <SiJavascript color='#b5a900' /> </li>
-              <li className='card-icon'> <FaPhp color='#0280ff' /> </li>
-              <li className='card-icon'> <SiMysql color='#0285ff' /> </li>
-            </div>
-            <div>
-              <h2>Asistentes Inmobiliarios</h2>
-              <span className='text-span-card'>Desarrollo Web</span>
-              <p>Trabajo Freelancer Registro de Usuarios y Contacto, se utilizo Php y MySql para almacenar los usuarios que se registren en la aplicacion.</p>
-            </div>
-            <div className='box-card-content_texts-links'>
-              <a href="https://github.com/Nahuxc/Book-Store-cursos" target='_blank' className='card-iconlink'><FaCode color='#009dff' /></a>
-              <a href="https://nahuxc.github.io/Book-Store-cursos/" target='_blank' className='card-iconlink'><AiOutlineGlobal color='#009dff' /></a>
-            </div>
-          </div>
-        </div>
-      </div>
       {/* card */}
       <div className='box-card'>
         <div className='box-card-content'>
@@ -121,12 +70,63 @@ const ContentProject = () => {
             </div>
             <div>
               <h2>NewsLetters Generator</h2>
-              <span className='text-span-card'> MERN | Aplicacion web ( en desarrollo... )</span>
+              <span className='text-span-card'> MERN | Aplicacion web </span>
               <p>Este Proyecto es un MERN STACK fue creado con React Vite, Node JS, Express JS, MongoDB , Esta conectado a la "Api rest Blog". Genera noticias que el usuario cree, pudiendo tambien editarlas, eliminarlas.</p>
             </div>
             <div className='box-card-content_texts-links'>
               <a href="https://github.com/Nahuxc/NewsLetters-MERN" target='_blank' className='card-iconlink'><FaCode color='#009dff' /></a>
               <a href="https://newslettersgenerator.netlify.app/" target='_blank' className='card-iconlink'><AiOutlineGlobal color='#009dff' /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* card */}
+      <div className='box-card'>
+        <div className='box-card-content'>
+          <div className='box-img'>
+            <img src={imgA} alt="bookstorecourseshome" />
+          </div>
+          <div className='box-card-content_texts'>
+            <div className='box-card-content_texts-icons'>
+              <li className='card-icon'> <FaHtml5 color='#ff430a' /> </li>
+              <li className='card-icon'> <FaCss3Alt color='#009dff' /> </li>
+              <li className='card-icon'> <SiJavascript color='#b5a900' /> </li>
+              <li className='card-icon'> <FaPhp color='#0280ff' /> </li>
+              <li className='card-icon'> <SiMysql color='#0285ff' /> </li>
+            </div>
+            <div>
+              <h2>Asistentes Inmobiliarios</h2>
+              <span className='text-span-card'>Desarrollo Web</span>
+              <p>Trabajo Freelancer Registro de Usuarios y Contacto, se utilizo Php y MySql para almacenar los usuarios que se registren en la aplicacion.</p>
+            </div>
+            <div className='box-card-content_texts-links'>
+              <a href="https://github.com/Nahuxc/Book-Store-cursos" target='_blank' className='card-iconlink'><FaCode color='#009dff' /></a>
+              <a href="https://asistentesinmobiliarios.com/" target='_blank' className='card-iconlink'><AiOutlineGlobal color='#009dff' /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* card */}
+      <div className='box-card'>
+        <div className='box-card-content'>
+          <div className='box-img'>
+            <img src={imgB} alt="bookstorecourseshome" />
+          </div>
+          <div className='box-card-content_texts'>
+            <div className='box-card-content_texts-icons'>
+              <li className='card-icon'> <FaSass color='#ff06ac' /> </li>
+              <li className='card-icon'> <SiJavascript color='#b5a900' /> </li>
+              <li className='card-icon'> <FaCss3Alt color='#009dff' /> </li>
+              <li className='card-icon'> <FaHtml5 color='#ff430a' /> </li>
+            </div>
+            <div>
+              <h2>Book Store Cursos</h2>
+              <span className='text-span-card'>Desarrollo Web</span>
+              <p>Maquetado Desarrollado con el FrameWork SASS de CSS, Demuestra conocimientos en el desarrollo de diseño web y algunas funcionalidades con JS. (No tiene responsive mobile)</p>
+            </div>
+            <div className='box-card-content_texts-links'>
+              <a href="https://github.com/Nahuxc/Book-Store-cursos" target='_blank' className='card-iconlink'><FaCode color='#009dff' /></a>
+              <a href="https://nahuxc.github.io/Book-Store-cursos/" target='_blank' className='card-iconlink'><AiOutlineGlobal color='#009dff' /></a>
             </div>
           </div>
         </div>
