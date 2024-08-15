5fc59a5e842c65da05a0728fa075d51c0caeb732
Nahuel <coronel98nahuel@gmail.com>
2024-08-15 15:52:26 -0300

    update proyects

--- src/Components/Projects/ContentProject/ContentProject.jsx
+++ src/Components/Projects/ContentProject/ContentProject.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import "./ContentProject.css"
 
 
-import imgA from "../../../../img/dvasistentesinmobiliarios.jpg"
+import imgA from "../../../../img/VIDEO-GAMES-WEB.png"
 import imgB from "../../../../img/bookstorecourseshome.jpg"
 import imgC from "../../../../img/api-articles.png"
 import imgD from "../../../../img/NewsLettersGenerator.png"
@@ -82,31 +82,30 @@ const ContentProject = () => {
         </div>
       </div>
       {/* card */}
-      {/* <div className='box-card'>
+      <div className='box-card'>
         <div className='box-card-content'>
           <div className='box-img'>
-            <img src={imgA} alt="bookstorecourseshome" />
+            <img src={imgA} alt="video-games-web" />
           </div>
           <div className='box-card-content_texts'>
             <div className='box-card-content_texts-icons'>
-              <li className='card-icon'> <FaHtml5 color='#ff430a' /> </li>
-              <li className='card-icon'> <FaCss3Alt color='#009dff' /> </li>
-              <li className='card-icon'> <SiJavascript color='#b5a900' /> </li>
               <li className='card-icon'> <FaPhp color='#0280ff' /> </li>
               <li className='card-icon'> <SiMysql color='#0285ff' /> </li>
+              <li className='card-icon'> <FaCss3Alt color='#009dff' /> </li>
+              <li className='card-icon'> <SiJavascript color='#b5a900' /> </li>
             </div>
             <div>
-              <h2>Asistentes Inmobiliarios</h2>
-              <span className='text-span-card'>Desarrollo Web</span>
-              <p>Trabajo Freelancer Registro de Usuarios y Contacto, se utilizo Php y MySql para almacenar los usuarios que se registren en la aplicacion.</p>
+              <h2>Video Juegos Web (En desarrollo)</h2>
+              <span className='text-span-card'>Desarrollo Web PHP</span>
+              <p>WEB con PHP y MySQL para registro y logueo de usuarios, basado en video juegos para que los usuarios puedan crear sus opiniones o posteos.</p>
             </div>
             <div className='box-card-content_texts-links'>
-              <a href="https://github.com/Nahuxc/Book-Store-cursos" target='_blank' className='card-iconlink'><FaCode color='#009dff' /></a>
-              <a href="https://asistentesinmobiliarios.com/" target='_blank' className='card-iconlink'><AiOutlineGlobal color='#009dff' /></a>
+              <a href="https://github.com/Nahuxc/videogamesWeb" target='_blank' className='card-iconlink'><FaCode color='#009dff' /></a>
+              <a href="" target='_blank' className='card-iconlink'><AiOutlineGlobal color='#009dff' /></a>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
       {/* card */}
       <div className='box-card'>
         <div className='box-card-content'>
